a0b7f4594d4d2ca8bd17fbff4f95ae6a77b1cb32
Colin Adler <colin@coder.com>
2019-08-20 21:37:12 -0500

    Async processing with tokio

--- main.rs
+++ main.rs
@@ -1,5 +1,11 @@
-extern crate redis;
+#![feature(async_await)]
 
+extern crate eetf;
+extern crate redis;
+extern crate tokio;
+
+
+#[allow(unused_imports)]
 use eetf::{Atom, Map, Term};
 use redis::Commands;
 use std::error::Error;
@@ -11,13 +17,21 @@ struct PlayedMessage {
     game: String,
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let rc = redis::Client::open("redis://127.0.0.1")?;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let rc = redis::Client::open("redis://127.0.0.1:6380")?;
     let mut conn = rc.get_connection()?;
 
     loop {
-        let cmd: Vec<u8> = conn.blpop("hello", 0)?;
-        let term = Term::decode(Cursor::new(&cmd))?;
-        println!("{}", term)
+        let cmd: (String, Vec<u8>) = conn.blpop("gateway:events:PRESENCE_UPDATE", 0)?;
+        let (_, mut raw) = cmd;
+
+        tokio::spawn(async move {
+            let mut cmdd: Vec<u8> = vec![131];
+            cmdd.append(&mut raw);
+            Term::decode(Cursor::new(&mut cmdd)).unwrap();
+        });
+
     }
 }
