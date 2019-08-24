0138cb1ec465949b0e70b0009d81042896f78b7a
Colin Adler <colin@coder.com>
2019-08-23 22:10:31 -0500

    It prints game structs

--- main.rs
+++ main.rs
@@ -1,23 +1,23 @@
-#![feature(async_await)]
-
 extern crate eetf;
+extern crate num;
 extern crate redis;
 extern crate tokio;
 
-use eetf::Term;
+use eetf::{Map, Term};
+use num::ToPrimitive;
 use redis::Commands;
-use std::error::Error;
 use std::io::Cursor;
 
 #[allow(dead_code)]
+#[derive(Debug)]
 struct PlayedMessage {
     user: i64,
     game: String,
 }
 
-
+#[allow(unused_variables)]
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let rc = redis::Client::open("redis://127.0.0.1:6380")?;
     let mut conn = rc.get_connection()?;
 
@@ -28,8 +28,72 @@ async fn main() -> Result<(), Box<dyn Error>> {
         tokio::spawn(async move {
             let mut cmdd: Vec<u8> = vec![131];
             cmdd.append(&mut raw);
-            Term::decode(Cursor::new(&mut cmdd)).unwrap();
-        });
+            let t: Term = Term::decode(Cursor::new(&mut cmdd)).unwrap();
 
+            let m = match t {
+                Term::Map(m) => m,
+                t => {
+                    println!("unknown type {}", t);
+                    return;
+                }
+            };
+
+            let mut pl = PlayedMessage {
+                user: 0,
+                game: String::from(""),
+            };
+
+            for i in m.entries {
+                if let Term::Atom(k) = i.0 {
+                    match k.name.as_ref() {
+                        "user" => {
+                            if let Term::Map(u) = i.1 {
+                                pl.user = find_id(u);
+                            }
+                        }
+                        "game" => {
+                            pl.game = find_game(i.1);
+                        }
+                        _ => {}
+                    }
+                }
+            }
+
+            dbg!(&pl);
+        });
     }
+}
+
+fn find_game(g: Term) -> String {
+    if let Term::Map(g) = g {
+        for i in g.entries {
+            if let Term::Atom(k) = i.0 {
+                if "name" == k.name {
+                    match i.1 {
+                        Term::Atom(game) => return game.name,
+                        Term::Binary(game) => return String::from_utf8(game.bytes).unwrap(),
+                        _ => println!("game type {}", &i.1),
+                    }
+                }
+            }
+        }
+    }
+
+    String::from("")
+}
+
+fn find_id(u: Map) -> i64 {
+    for i in u.entries {
+        if let Term::Atom(g) = i.0 {
+            if g.name == "id" {
+                if let Term::BigInteger(big) = i.1 {
+                    if let Some(id) = big.value.to_i64() {
+                        return id;
+                    }
+                }
+            }
+        }
+    }
+
+    0
 }
