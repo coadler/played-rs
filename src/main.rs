34891e0e2ba052e6bd47a6114a01d64b7c674bd3
Colin Adler <cadler@coder.com>
2019-11-28 15:52:18 -0600

    more bs

--- src/main.rs
+++ src/main.rs
@@ -16,43 +16,55 @@ extern crate ws;
 use foundationdb::*;
 use futures::future::*;
 use played::Presence;
-use ws::listen;
+use std::error::Error;
+use std::sync::Arc;
 
 #[allow(dead_code)]
 #[allow(unused_mut)]
 #[allow(unused_variables)]
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let network = foundationdb::boot().expect("failed to init boot network");
-    let db = foundationdb::Database::default().expect("failed to open fdb");
+async fn main() -> Result<(), Box<dyn Error>> {
+    let mut played = Played::new();
 
-    listen("127.0.0.1:8080", |out| handler).expect("failed to handle ws");
+    ws::listen("127.0.0.1:8080", |out| played.clone().handler()).expect("failed to handle ws");
     Ok(())
 }
 
-struct Played<'a> {
-    db: &'a Database,
+struct Played {
+    db: Database,
 }
 
-fn handler(msg: ws::Message) -> ws::Result<()> {
-    let mut data: Vec<u8> = vec![131];
-    data.append(msg.into_data().as_mut());
-    let res: Presence = serde_eetf::from_bytes(&data).unwrap();
-    dbg!(res);
+impl Played {
+    fn new() -> Arc<Played> {
+        foundationdb::boot().expect("failed to init boot network");
+        let db = foundationdb::Database::default().expect("failed to open fdb");
 
-    // tokio::spawn(process(db));
+        Arc::new(Played { db })
+    }
 
-    Ok(())
-}
+    async fn process(&self, p: Presence) {
+        self.db
+            .transact(
+                (),
+                |txn: &Transaction, ()| test(txn).boxed_local(),
+                TransactOption::default(),
+            )
+            .await
+            .unwrap();
+    }
 
-async fn process(db: Database) {
-    db.transact(
-        (),
-        |txn: &Transaction, ()| test(txn).boxed_local(),
-        TransactOption::default(),
-    )
-    .await
-    .unwrap();
+    fn handler(&self) -> Box<dyn Fn(ws::Message) -> Result<(), ws::Error>> {
+        Box::new(|msg| {
+            let mut data: Vec<u8> = vec![131];
+            data.append(msg.into_data().as_mut());
+            let res: Presence = serde_eetf::from_bytes(&data).unwrap();
+            dbg!(res);
+
+            let f = self.process(res).boxed();
+            tokio::spawn(f);
+            Ok(())
+        })
+    }
 }
 
 async fn test(txn: &Transaction) -> FdbResult<()> {
