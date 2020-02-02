cad14517a6eadfcaf1672e555d44649a4c6e36dc
Colin Adler <colin@coder.com>
2020-02-01 18:44:39 -0600

    Use UnsafeCell

--- src/main.rs
+++ src/main.rs
@@ -7,49 +7,75 @@ use byteorder::{ByteOrder, LittleEndian};
 use chrono::Utc;
 use foundationdb::*;
 use futures::future::*;
+use futures::{stream::TryStreamExt, StreamExt};
 use played::Presence;
+use std::cell::UnsafeCell;
+use std::env;
 use std::error::Error;
-use std::ptr;
-use std::sync::Arc;
+use tokio::net::{TcpListener, TcpStream};
 
-const global_fdb: *const Database = ptr::null();
-const global_rdb: *const i32 = ptr::null();
+struct Server {
+    fdb: UnsafeCell<Database>,
+}
+
+unsafe impl Sync for Server {}
+unsafe impl Send for Server {}
 
 #[allow(dead_code)]
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    unsafe {
-        foundationdb::boot().expect("failed to init boot network");
-        let db = foundationdb::Database::default().expect("failed to open fdb");
-        *global_fdb = db;
+    foundationdb::boot().expect("failed to init boot network");
+    let db: Database = foundationdb::Database::default().expect("failed to open fdb");
 
-        ws::listen("127.0.0.1:8080", |out| handler()).expect("failed to handle ws");
+    let p: *const Server = &Server {
+        fdb: UnsafeCell::new(db),
+    };
+
+    let addr = env::args()
+        .nth(1)
+        .unwrap_or_else(|| "127.0.0.1:8080".to_string());
+
+    let try_socket = TcpListener::bind(&addr).await;
+    let mut listener = try_socket.expect("Failed to bind");
+    println!("Listening on: {}", addr);
+
+    while let Ok((stream, _)) = listener.accept().await {
+        unsafe {
+            tokio::spawn((*p).accept(stream));
+        }
     }
+
     Ok(())
 }
 
-fn handler() -> Box<dyn Fn(ws::Message) -> ws::Result<()>> {
-    Box::new(move |msg: ws::Message| -> ws::Result<()> {
-        let mut data: Vec<u8> = vec![131];
-        data.append(msg.into_data().as_mut());
-        let res: Presence = serde_eetf::from_bytes(&data).unwrap();
-        dbg!(&res);
-
-        unsafe {
-            tokio::spawn(process(res));
-        }
-        Ok(())
-    })
-}
-
-async fn process(pres: Presence) {
-    unsafe {
-        ptr::read(global_fdb.as_ref().unwrap())
+impl Server {
+    async unsafe fn process(&self, pres: Presence) {
+        let db: *const Database = self.fdb.get();
+        (*db)
             .transact_boxed(
                 pres,
                 |txn: &Transaction, pres| exec(txn, pres).boxed(),
                 TransactOption::default(),
             )
+            .await
+            .unwrap()
+    }
+
+    async unsafe fn accept(&self, stream: TcpStream) {
+        let mut buf: Vec<u8> = vec![];
+        let ws_stream = tokio_tungstenite::accept_async(stream)
+            .await
+            .expect("Error during the websocket handshake occurred");
+
+        ws_stream
+            .try_for_each(|msg| {
+                buf.truncate(0);
+                buf.push(131);
+                buf.append(msg.into_data().as_mut());
+                let res: Presence = serde_eetf::from_bytes(&buf).unwrap();
+                tokio::spawn(self.process(res));
+                ok(())
+            })
             .await
             .unwrap();
     }
